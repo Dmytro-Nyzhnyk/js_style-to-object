73aef850c59e9a2a425ee5dd6ae20c690b6d1628
Dmytro Nizhnyk <nothan89@gmail.com>
2025-05-08 18:56:01 +0300

    solution for js_style-to-objec

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayProperties = sourceString.split(';');
+
+  const checkedArr = arrayProperties.filter((value) => /[a-zA-Z]/.test(value));
+
+  const obj = {};
+
+  for (let i = 0; i < checkedArr.length; i++) {
+    const valueArr = checkedArr[i].split(':');
+
+    const keyObj = valueArr[0].trim();
+    const valueObj = valueArr[1].trim();
+
+    obj[keyObj] = valueObj;
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;
